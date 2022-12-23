b6be093787249183cf49b8314534ee50033aded1
xxxyifan <100944142+xxxyifan@users.noreply.github.com>
2022-12-23 13:52:17 +0800

    Add files via upload

---
+++ project/gym_game/envs/cus_env.py
@@ -0,0 +1,32 @@
+import gym
+from gym import spaces
+import numpy as np
+from gym_game.envs.game_v0 import Gym_Game
+
+class CustomEnv(gym.Env):
+    #metadata = {'render.modes' : ['human']}
+    def __init__(self):
+        self.pygame = Gym_Game()
+        self.action_space = spaces.Discrete(120)
+        self.observation_space = spaces.Box(np.zeros((10,)), np.full((10,), 10), dtype=np.float64)
+
+    def reset(self):
+        del self.pygame
+        self.pygame = Gym_Game()
+        obs = self.pygame.observe()
+        return obs
+
+    def step(self, action):
+        print("action start, action = ", action)
+        self.pygame.action(action)
+        print("action finish, observe start")
+        obs = self.pygame.observe()
+        print("observe finish, evaluate start")
+        reward = self.pygame.evaluate()
+        print("evaluate finish, is_done start, reward = ", reward)
+        done = self.pygame.is_done()
+        print("is_done finish, return to main")
+        return obs, reward, done, {}, {}
+
+    def render(self, mode="human", close=False):
+        self.pygame.view()
